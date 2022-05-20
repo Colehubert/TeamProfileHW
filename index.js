05e93dfe7373ddec55c7604e7957c0f977201956
Colehubert <Colehubert2001@gmail.com>
2022-05-20 07:47:07 -0700

    adding more small stuff

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function intern(){inquirer.prompt({type:"input",name:"intern_name",message:"what
 {type:"input",name:"email",message:"what is the interns email?"},
 {type:"input",name:"school",message:"what school does the intern go too ?"}).then((answers)=>{const intern = new intern(answers.internname,answers.id,answers.email,answers.school)},team_members.push(intern))}
 
+
 function engineer(){inquirer.prompt({type:"input",name:"engineer_name",message:"what is the engineers name ?"},
 {type:"input",name:"id",message:"What is the engineers id?"},
 {type:"input",name:"email",message:"what is the engineers email?"},
@@ -12,7 +13,7 @@ function engineer(){inquirer.prompt({type:"input",name:"engineer_name",message:"
 
 
 
-function engineer(){inquirer.prompt({type:"input",name:"manager_name",message:"what is the managers name ?"},
+function manager(){inquirer.prompt({type:"input",name:"manager_name",message:"what is the managers name ?"},
 {type:"input",name:"id",message:"What is the managers id?"},
 {type:"input",name:"email",message:"what is the managers email?"},
 {type:"input",name:"school",message:"what school does the managers go too ?"}).then((answers)=>{const manager = new manager(answers.managername,answers.id,answers.email,answers.school)},team_members.push(manager))}
